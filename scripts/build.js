9bceb53950e691b3cdef7b4b5bac868640740ebd
chengpeiquan <chengpeiquan@chengpeiquan.com>
2025-03-16 23:43:49 +0800

    fix: build task on ci

--- scripts/build.js
+++ scripts/build.js
@@ -2,20 +2,70 @@ import { execSync } from 'node:child_process'
 import { existsSync, mkdirSync } from 'node:fs'
 import { join } from 'node:path'
 
+const WIN_OUTPUT_NAME = 'Git_Commit_Analytics_win.exe'
+const MAC_OUTPUT_NAME = 'Git_Commit_Analytics_mac'
+const SEA_BLOB = 'sea-prep.blob'
+const OUTPUT_DIR = 'dist'
+
+const NODE_PATH = process.execPath // Node.js executable file path
+
+async function buildWin() {
+  const OUTPUT_PATH = join(OUTPUT_DIR, WIN_OUTPUT_NAME)
+
+  try {
+    console.log(`📦 Creating standalone executable for Windows...`)
+
+    execSync(
+      `node -e "require('fs').copyFileSync(process.execPath, ${OUTPUT_PATH})" `,
+    )
+
+    execSync(`signtool remove /s ${OUTPUT_PATH}`)
+
+    execSync(
+      [
+        `npx postject ${OUTPUT_PATH} NODE_SEA_BLOB ${SEA_BLOB}`,
+        '--sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2',
+      ].join(' '),
+    )
+
+    execSync(`codesign --sign - ${OUTPUT_PATH}`)
+    return true
+  } catch (error) {
+    console.error('❌ Failed to create executable for Windows:', error)
+    return false
+  }
+}
+
+async function buildMac() {
+  const OUTPUT_PATH = join(OUTPUT_DIR, MAC_OUTPUT_NAME)
+
+  try {
+    console.log(`📦 Creating standalone executable for macOS...`)
+
+    execSync(`cp ${NODE_PATH} ${OUTPUT_PATH}`)
+
+    execSync(`codesign --remove-signature ${OUTPUT_PATH}`)
+
+    execSync(
+      [
+        `npx postject ${OUTPUT_PATH} NODE_SEA_BLOB ${SEA_BLOB}`,
+        '--sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2',
+        '--macho-segment-name NODE_SEA',
+      ].join(' '),
+    )
+
+    execSync(`codesign --sign - ${OUTPUT_PATH}`)
+    return true
+  } catch (error) {
+    console.error('❌ Failed to create executable for macOS:', error)
+    return false
+  }
+}
+
 // https://nodejs.org/api/single-executable-applications.html
 async function build() {
-  const WIN_OUTPUT_NAME = 'Git_Commit_Analytics_win.exe'
-  const MAC_OUTPUT_NAME = 'Git_Commit_Analytics_mac'
-  const SEA_BLOB = 'sea-prep.blob'
-  const OUTPUT_DIR = 'dist'
-
   const PLATFORM = process.platform // 'win32' | 'darwin' | 'linux'
   const IS_WIN = PLATFORM === 'win32'
-  const IS_MAC = PLATFORM === 'darwin'
-
-  const OUTPUT_NAME = IS_WIN ? WIN_OUTPUT_NAME : MAC_OUTPUT_NAME
-  const OUTPUT_PATH = join(OUTPUT_DIR, OUTPUT_NAME)
-  const NODE_PATH = process.execPath // Node.js executable file path
 
   if (!existsSync(OUTPUT_DIR)) {
     mkdirSync(OUTPUT_DIR, { recursive: true })
@@ -36,40 +86,14 @@ async function build() {
     process.exit(1)
   }
 
-  console.log(`📦 Creating standalone executable for ${PLATFORM}...`)
+  const buildTask = IS_WIN ? buildWin : buildMac
 
-  try {
-    if (IS_WIN) {
-      execSync(
-        `cmd /c "copy /b ${NODE_PATH} + ${SEA_BLOB} ${OUTPUT_PATH} && exit /b"`,
-        {
-          stdio: 'inherit',
-          shell: true,
-        },
-      )
-    } else {
-      execSync(`cp ${NODE_PATH} ${OUTPUT_PATH}`)
-
-      if (IS_MAC) {
-        execSync(`codesign --remove-signature ${OUTPUT_PATH}`)
-
-        execSync(
-          [
-            `npx postject ${OUTPUT_PATH} NODE_SEA_BLOB ${SEA_BLOB}`,
-            '--sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2',
-            '--macho-segment-name NODE_SEA',
-          ].join(' '),
-        )
-
-        execSync(`codesign --sign - ${OUTPUT_PATH}`)
-      }
-    }
-  } catch (error) {
-    console.error('❌ Failed to create executable:', error)
+  const isSuccess = await buildTask()
+  if (!isSuccess) {
     process.exit(1)
   }
 
-  console.log(`✅ Build complete: ${OUTPUT_PATH}`)
+  console.log(`✅ Build complete!`)
 }
 
 build().catch(console.error)
