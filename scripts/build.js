fe8cef7b05c34936bf9877c82179d41cff6ead4a
chengpeiquan <chengpeiquan@chengpeiquan.com>
2025-03-17 00:13:32 +0800

    fix: build error

--- scripts/build.js
+++ scripts/build.js
@@ -25,7 +25,7 @@ async function buildWin() {
 
     execSync(
       [
-        `npx postject ${OUTPUT_PATH} NODE_SEA_BLOB ${SEA_BLOB}`,
+        `postject ${OUTPUT_PATH} NODE_SEA_BLOB ${SEA_BLOB}`,
         '--sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2',
       ].join(' '),
     )
@@ -50,7 +50,7 @@ async function buildMac() {
 
     execSync(
       [
-        `npx postject ${OUTPUT_PATH} NODE_SEA_BLOB ${SEA_BLOB}`,
+        `postject ${OUTPUT_PATH} NODE_SEA_BLOB ${SEA_BLOB}`,
         '--sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2',
         '--macho-segment-name NODE_SEA',
       ].join(' '),
